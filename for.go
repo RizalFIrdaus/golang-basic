65466112bea208a84738997589ce5b0aa6b6a244
rizal300500 <rizal.firdaus117@gmail.com>
2023-11-20 00:30:04 +0700

    for looping

---
+++ for.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//for counter := 1; counter <= 100; counter++ {
+	//	fmt.Println("Perulangan ke ", counter)
+	//}
+	//
+	//fmt.Println("Selesai")
+
+	// For Range
+	names := []string{"Muhammad", "Rizal", "Firdaus"}
+
+	for i := 0; i < len(names); i++ {
+		fmt.Println(i+1, names[i])
+	}
+	fmt.Println("")
+	for i, name := range names {
+		fmt.Println(i+1, name)
+	}
+	fmt.Println("")
+	for _, name := range names {
+		fmt.Println(name)
+	}
+
+}
