38cb5264a734ed8d047723b4d95e8b3ab6b64a80
rizal300500 <rizal.firdaus117@gmail.com>
2023-11-19 20:19:47 +0700

    switch case

--- switch.go
+++ switch.go
@@ -1,8 +1,6 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
 	const isPassedValue = 78
@@ -11,31 +9,32 @@ func main() {
 		1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
 	}
 
-	uts := 76
-	uas := 90
+	uas := 70
+	uts := 80
 
-	finalScore := calculateFinalScore(absent, uts, uas)
+	finalScore := calculateFinalScoreSwitch(absent, uts, uas)
 
-	if finalScore == 100 {
+	switch {
+	case finalScore == 100:
 		fmt.Println("Nilai kamu sempurna, nilai kamu :", finalScore)
-	} else if finalScore >= 90 && finalScore < 100 {
+	case finalScore >= 90 && finalScore < 100:
 		fmt.Println("Nilai kamu hampir sempurna, nilai kamu :", finalScore)
-	} else if finalScore >= isPassedValue && finalScore < 90 {
+	case finalScore >= isPassedValue && finalScore < 90:
 		fmt.Println("Nilai kamu cukup baik, nilai kamu :", finalScore)
-	} else {
+	default:
 		fmt.Println("Kamu perlu ujian ulang agar kamu bisa lulus, nilai kamu :", finalScore)
 	}
 
 }
 
-func calculateFinalScore(absent [20]int, uts int, uas int) int {
+func calculateFinalScoreSwitch(absent [20]int, uts int, uas int) int {
 
-	scoreAbsent := countAbsentScore(absent) * 5
+	scoreAbsent := countAbsentScoreSwitch(absent) * 5
 
 	return (scoreAbsent + uts + uas) / 3
 }
 
-func countAbsentScore(absent [20]int) int {
+func countAbsentScoreSwitch(absent [20]int) int {
 	sum := 0
 
 	for _, value := range absent {
